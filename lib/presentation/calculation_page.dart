e8701dd012574b0a789d2a3a12359401bbd0e553
Felix Blessing <felix.blessing@outlook.de>
2021-11-19 15:23:00 +0100

    Projekt hinzufügen

---
+++ lib/presentation/calculation_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class CalculationPage extends StatelessWidget {
+  const CalculationPage({ Key? key }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calculator"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              color: Colors.white,
+            )),
+            Flexible(
+              flex: 3,
+              child: Container(
+                color: Colors.grey[900],
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 25, left: 9, right: 9),
+                  child: GridView.count(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 12,
+                    crossAxisSpacing: 12,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: [
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.pink[100],child: Text("AC", style: TextStyle(fontSize: 30, color: Colors.black),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.green[100],child: Text("()", style: TextStyle(fontSize: 30, color: Colors.black),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.green[100],child: Text("%", style: TextStyle(fontSize: 30, color: Colors.black),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.green[100],child: Text("%", style: TextStyle(fontSize: 30, color: Colors.black),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.grey[850],child: Text("7", style: TextStyle(fontSize: 30,),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.grey[850],child: Text("8", style: TextStyle(fontSize: 30,),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.grey[850],child: Text("9", style: TextStyle(fontSize: 30,),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.green[100],child: Text("x", style: TextStyle(fontSize: 30, color: Colors.black),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.grey[850],child: Text("4", style: TextStyle(fontSize: 30,),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.grey[850],child: Text("5", style: TextStyle(fontSize: 30,),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.grey[850],child: Text("6", style: TextStyle(fontSize: 30,),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.green[100],child: Text("-", style: TextStyle(fontSize: 30, color: Colors.black),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.grey[850],child: Text("1", style: TextStyle(fontSize: 30,),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.grey[850],child: Text("2", style: TextStyle(fontSize: 30,),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.grey[850],child: Text("3", style: TextStyle(fontSize: 30,),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.green[100],child: Text("+", style: TextStyle(fontSize: 30, color: Colors.black),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.grey[850],child: Text("0", style: TextStyle(fontSize: 30,),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.grey[850],child: Text(",", style: TextStyle(fontSize: 30,),),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.grey[850],child: Icon(Icons.backspace_outlined),),
+                      FloatingActionButton(onPressed: (){}, backgroundColor: Colors.blue[50],child: Text("=", style: TextStyle(fontSize: 30, color: Colors.black),),),
+                    ],
+                    ),
+                )
+              ))
+        ],
+        
+      ),
+        
+    );
+  }
+}
