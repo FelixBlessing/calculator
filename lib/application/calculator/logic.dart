69a8bc77bd0bce0e9768f47933f44a3f31ec8317
Felix Blessing <felix.blessing@outlook.de>
2021-12-07 14:59:31 +0100

    Fertigstellung

--- lib/application/calculator/logic.dart
+++ lib/application/calculator/logic.dart
@@ -1,1 +1,31 @@
-class Logic{}
+class Logic{
+
+  String calculate(number1, number2, operator){
+    double ergebnis = 0;
+    double a = double.parse(number1);
+    double b = double.parse(number2);
+
+    switch(operator){
+      case "+":
+        ergebnis = a+b;
+        return ergebnis.toString();
+      
+      case "-":
+        ergebnis = a-b;
+        return ergebnis.toString();
+      
+      case "x":
+        ergebnis = a*b;
+        return ergebnis.toString();
+      
+      case "/":
+        ergebnis = a/b;
+        return ergebnis.toString();
+      
+      default:
+        return ergebnis.toString();
+    }
+  }
+
+
+}
